966eb8213256c9deb8dc77d782b59afc6769fbc4
musiienko25 <devmusiienko@gmail.com>
2024-11-10 16:47:35 +0200

    added cruds

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.module';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {
+
+    }
+    @Get()   
+    getTasks() {
+      return this.tasksService.getAllTasks()
+    }
+    @Post()
+    createTask(@Body('title') title: string, @Body('description') description: string) : Task {
+        return this.tasksService.createTask(title, description)
+    }
+
+    @Get()
+    getTaskByid(@Param('id') id: string) {
+        return this.tasksService.getTaskByid(id)
+    }
+
+    
+}
